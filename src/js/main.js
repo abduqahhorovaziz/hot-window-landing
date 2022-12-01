c3c9492e6d1aeeb1c049bb1074d9954b49f918a2
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-12-01 18:36:29 +0500

    all done

--- src/js/main.js
+++ src/js/main.js
@@ -56,4 +56,32 @@ let swiperGrid = new Swiper(".gridSwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
 });
+
+const openNavBtn = document.querySelector("#openNavBtn");
+const mobileNav = document.querySelector("#mobileNav");
+const closeNavBtn = document.querySelector("#closeBtn");
+
+openNavBtn.onclick = function () {
+  mobileNav.classList.remove("hidden");
+  mobileNav.classList.add("block");
+};
+
+closeNavBtn.onclick = function () {
+  mobileNav.classList.add("hidden");
+  mobileNav.classList.remove("block");
+};
