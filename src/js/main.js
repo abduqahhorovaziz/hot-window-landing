a795d1617e4d1c38d3aa88b092e9e2b2825239c4
abduqahhorovaziz <aziz.abduqahhorov6111@gmail.com>
2022-12-01 08:05:34 +0500

    feat: full site is ready without responsive

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,42 @@ let swiper = new Swiper(".mySwiper", {
   },
 });
 
+let salesSwiper = new Swiper(".salesSwiper", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".sales-pagination",
+    type: "bullets",
+    bulletElement: "div",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+
+let salesSwiperSecond = new Swiper(".salesSwiperSecond", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".sales-pagination",
+    type: "bullets",
+    bulletElement: "div",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+
 let swiperGrid = new Swiper(".gridSwiper", {
+  autoplay: {
+    delay: 3000,
+  },
   slidesPerView: 3,
   spaceBetween: 30,
   pagination: {
